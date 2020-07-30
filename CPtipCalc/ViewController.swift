af82d48de312370cb56420ad3278d3e3da81be31
Min Kyaw <minhhkyaw@gmail.com>
2020-07-30 04:15:22 -0700

    Initial Commit

---
+++ CPtipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CPtipCalc
+//
+//  Created by Min Kyaw on 7/30/20.
+//  Copyright © 2020 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
